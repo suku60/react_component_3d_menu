bc7fda4b097a7ead599e272118b2f082c5d49eea
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-05-29 14:37:21 +0200

    Added menu component. Deleted useless stuff from react app.

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import './Menu.css';
+
+const Menu = () => {
+
+    
+
+  return (
+    <div>
+            menu
+        
+    </div>
+    )
+}
+export default Menu;
